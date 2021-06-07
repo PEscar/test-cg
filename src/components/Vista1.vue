baf3138f2f905723781a6f9fde252cbcde5b8437
PEscar <pedroscarselletta@gmail.com>
2021-06-07 15:48:51 +0200

    filters added

--- src/components/Vista1.vue
+++ src/components/Vista1.vue
@@ -1,26 +1,8 @@
 <template>
   <div class="hello">
-    <button @click="loadRandomUsers()">Consultar</button>
+    <!-- <button @click="loadRandomUsers()">Consultar</button> -->
 
-    {{ randomUsers[0] }}
-
-<!--     <table>
-      <thead>
-        <th>Género</th>
-        <th>Nombre</th>
-        <th>Email</th>
-        <th>Nacionalidad</th>
-        <th>Fecha de Nacimiento</th>
-        <th>Fecha de Registro</th>
-      </thead>
-      <tbody>
-        <tr>
-          <td colspan="5">asd</td>
-        </tr>
-      </tbody>
-    </table> -->
-
-    <v-client-table ref="myTable" :data="randomUsers" :columns="columns" :options="tableOptions">
+    <v-client-table ref="myTable" :data="filteredRandomUsers" :columns="columns" :options="tableOptions">
 
       <template slot="name" slot-scope="data">
         {{ data.row.name.first }} {{ data.row.name.last }}
@@ -29,10 +11,23 @@
     </v-client-table>
 
     <template>
+      <button v-if="page > 1" @click="page = page - 1"><< {{ page - 1 }}</button>
+      &nbsp;<label for="page">Página {{page}}</label>&nbsp;
+      <button @click="page = page + 1">{{ page + 1 }} >></button>
+    </template>
+
+    <template>
       <h1>Filtros</h1>
 
-      
-      
+      <label for="genderFilter">Género</label>
+      <v-select id="genderFilter" @input="loadRandomUsers" ref="genderFilter" v-model="genderFilter" :options="genderFilterOptions" label="label" :reduce="option => option.value"></v-select>
+
+      <label for="nationalityFilter">Nacionalidad</label>
+      <v-select id="nationalityFilter" @input="loadRandomUsers" ref="nationalityFilter" v-model="nationalityFilter" :options="nationalitiesFilterOptions" label="label" :reduce="option => option.value"></v-select>
+
+      <label for="ageFilter">Edad</label>
+      <input type="number" class="form-control" v-model="ageFilter" value=''>
+
     </template>
 
   </div>
@@ -43,30 +38,68 @@ export default {
   name: 'Vista1',
   data(){
       return {
-          randomUsers: [],
-          columns: ['gender', 'name', 'email', 'nat', 'dob.date', 'registered.date'],
-          page: 1,
-          results: 10,
-          tableOptions: {
-            // sortIcon: { base:'bi', up:'bi-sort-down', down:'bi-sort-up', is:'bi-sort' },
-            headings: {
-              gender: 'Género',
-              name: 'Nombre',
-              email: 'Email',
-              nat: 'Nacionalidad',
-              'dob.date': 'Fecha de Nacimiento',
-              'registered.date': 'Fecha de Registro',
-            }
-          },
+        genderFilter: '',
+        genderFilterOptions: [{label: 'Todos', value: ''}, {label: 'Hombres', value: 'male'}, {label: 'Mujeres', value: 'female'}],
+
+        nationalityFilter: '',
+        nationalitiesFilterOptions: [{label: 'Todas', value: ''}, {label: 'AU', value: 'AU'}, {label: 'BR', value: 'BR'}, {label: 'CA', value: 'CA'}, {label: 'CH', value: 'CH'}, {label: 'DE', value: 'DE'}, {label: 'DK', value: 'DK'}, {label: 'ES', value: 'ES'}, {label: 'FI', value: 'FI'}, {label: 'FR', value: 'FR'}, {label: 'GB', value: 'GB'}, {label: 'IE', value: 'IE'}, {label: 'IR', value: 'IR'}, {label: 'NO', value: 'NO'}, {label: 'NL', value: 'NL'}, {label: 'NZ', value: 'NZ'}, {label: 'TR', value: 'TR'}, {label: 'US', value: 'US'}],
+
+        ageFilter: '',
+
+        randomUsers: [], // table rows
+        filteredRandomUsers: [], // results filtered by age
+        columns: ['gender', 'name', 'email', 'nat', 'dob.date', 'registered.date', 'dob.age'], // table columns
+        page: 1, // initial page
+        results: 10, // page size
+
+        tableOptions: { // table options
+          headings: {
+            gender: 'Género',
+            name: 'Nombre',
+            email: 'Email',
+            nat: 'Nacionalidad',
+            'dob.date': 'Fecha de Nacimiento',
+            'registered.date': 'Fecha de Registro',
+          }
+        },
       }
   },
+
+  created: function(){
+    this.loadRandomUsers()
+  },
+
+  watch:
+  {
+    page: function()
+    {
+      this.loadRandomUsers()
+    },
+
+    ageFilter: function(newAgeFilter)
+    {
+      if ( newAgeFilter )
+      {
+        this.filteredRandomUsers = this.randomUsers.filter(user => user.dob.age == newAgeFilter)
+      }
+      else
+      {
+        this.loadRandomUsers()
+      }
+    },
+
+    randomUsers: function(newRandomUsers)
+    {
+      this.filteredRandomUsers = newRandomUsers
+    }
+  },
+
   methods:
   {
     loadRandomUsers: function ()
     {
-      console.log('loadRandomUsers')
       this.axios
-        .get('https://randomuser.me/api?page=' + this.page + '&results=' + this.results)
+        .get('https://randomuser.me/api?page=' + this.page + '&results=' + this.results + '&gender=' + this.genderFilter + '&nat=' + this.nationalityFilter)
         .then(response => (this.randomUsers = response.data.results))
     }
   }
--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ import './registerServiceWorker'
 import {ClientTable, Event} from 'vue-tables-2';
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import vSelect from "vue-select";
 
+import "vue-select/dist/vue-select.css";
+
+Vue.component("v-select", vSelect);
 Vue.config.productionTip = false
 Vue.use(ClientTable, {}, false, 'bootstrap3')
 Vue.use(VueAxios, axios)
